d9d0614b699b1c2ca1b85e8d4daed206f8b42a2d
sandip <sandip@gmail.com>
2024-10-30 18:08:58 +0530

    Second Version of HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -3,6 +3,11 @@ public class HelloWord{
 public static void main (String[] args){
 
 System.out.println("Hello Developers!Welcome to Java world");
-
+displayName();
 }
+
+public stati void displayName(){
+System.out.println("My Name Is RobertSandweek");
+}
+
 }
